3ee105d2b58aecbfca80c5c846106d56da0364f4
Ryotaro Furu <ryotrzs@gmail.com>
2019-08-26 18:19:04 +0900

    upload

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth')
+
+// welcome page
+router.get('/', (req, res) => {
+    res.render('welcome');
+});
+
+// dashboard
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    res.render('dashboard', {
+        name: req.user.name
+    });
+});
+
+// ------test------
+router.get('/profile', ensureAuthenticated, (req, res) => {
+    res.render('profile', {
+        name: req.user.name
+    });
+});
+
+
+module.exports = router;
